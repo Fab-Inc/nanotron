793683b3faad2745ff16b6519c338855f0a16878
Oliver Garrod <oliver.garrod@fabdata.io>
2026-04-06 12:04:47 +0000

    Identified root cause of the per-dataset consumed tokens not accumumlating from resume point. It seems like this is onl used for logging puprposes so not critical, but same fix applied to offsets_in_samples, which could actually affect indexing with multi-stage data stages containing same dataset in each stage. Doesn't affect anything we have done up to now though

--- src/nanotron/data/nemo_dataset/blendable_dataset.py
+++ src/nanotron/data/nemo_dataset/blendable_dataset.py
@@ -121,7 +121,10 @@ class BlendableDataset(torch.utils.data.Dataset):
             # find idx of dataset that matches the folder path
             for idx, dataset in enumerate(datasets):
                 for folder_path, consumed_tokens in consumed_tokens_per_dataset_folder.items():
-                    if dataset.folder_path == folder_path:
+                    ds_folder_path = dataset.folder_path
+                    if hasattr(ds_folder_path, "path"):
+                        ds_folder_path = ds_folder_path.path
+                    if ds_folder_path == folder_path:
                         self.consumed_tokens[idx] = consumed_tokens
                         log_rank(f"[BlendableDataset] Setting consumed_tokens for dataset {idx} ({dataset.folder_path}) to {consumed_tokens}", logger=logger, level=logging.INFO, rank=0)
         
@@ -132,7 +135,10 @@ class BlendableDataset(torch.utils.data.Dataset):
         if offsets_in_samples is not None:
             for idx, dataset in enumerate(datasets):
                 for folder_path, offset in offsets_in_samples.items():
-                    if dataset.folder_path == folder_path:
+                    ds_folder_path = dataset.folder_path
+                    if hasattr(ds_folder_path, "path"):
+                        ds_folder_path = ds_folder_path.path
+                    if ds_folder_path == folder_path:
                         self.offsets_in_samples[idx] = offset
                         log_rank(f"[BlendableDataset] Applying offset {offset} samples to dataset {idx} ({dataset.folder_path})", logger=logger, level=logging.INFO, rank=0)
 
